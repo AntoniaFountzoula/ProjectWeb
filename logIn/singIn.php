b41c219e1283b1aaf6f7a7af0aad532933a4b616
AntoniaFntz <95241834+AntoniaFountzoula@users.noreply.github.com>
2022-09-02 14:57:49 +0300

    It checks the type of the device when the admin login

--- logIn/singIn.php
+++ logIn/singIn.php
@@ -19,25 +19,34 @@ if (isset($_POST['Login'])) {
     $Password = $_REQUEST['password'];
     $query = "SELECT * FROM user WHERE username='$Username' AND password='$Password' LIMIT 1";
     $results = mysqli_query($conn, $query);
+    $user_agent = $_SERVER['HTTP_USER_AGENT'];
 
     if (mysqli_num_rows($results) == 1)
     {
         // check if user is admin or user
         $logged_in_user = mysqli_fetch_assoc($results);
-
         switch ($logged_in_user['user_type'])
         {
-            case 'Admin':
-                $_SESSION['user'] = $logged_in_user['username'];
-                $_SESSION['Id']=$logged_in_user['user_id'];
-                header('Location:../AdminMenu/index.html');
-               // echo "<script>window.location.href='../AdminMenu/index.html';</script>";
+            case 'admin':
+              if(strpos($user_agent,'Mobi') == false)
+              {
+                    $_SESSION['user'] = $logged_in_user['username'];
+                    $_SESSION['Id'] = $logged_in_user['user_id'];
+                    header('Location: ../AdminMenu/index.html');
+              }
+              else
+              {
+                    echo "<script>                 
+                    alert('Please connect from desktop!');
+                    window.location.href='logIn.html';
+                    </script>";
+                    mysqli_close($conn);
+                }
                 break;
             case 'user':
                 $_SESSION['user'] = $logged_in_user['username'];
                 $_SESSION['Id']=$logged_in_user['user_id'];
                 header('Location:../UserMenu/UserMenu.php');
-                //echo "<script>window.location.href='../UserMenu/UserMenu.php';</script>";
                 break;
         }
     }
