0cb25f69b5ce227678761d91c07dcf8dfd6d073d
unknown <olgakivretakou@gmail.com>
2022-09-06 17:39:54 +0300

    modal pretty please

--- UserMenu/UserMenu.php
+++ UserMenu/UserMenu.php
@@ -78,13 +78,47 @@ if (!isset($_SESSION['user']))
             </button>
             </span>
         </div>
-
-
-        <div class=" w-75 p-3">
+<div class="container">
+    <div class="row">
+        <div class="col-md-9">
+        <div class=" w-75 p-2">
             <div id="map"  style="width: 960px; height: 650px; "></div>
         </div>
+    </div>
+        <div class="col-md-3">
+            <div class="position-relative">
+                <div class="position-absolute top-0 start-50 translate-middle-x">
+                <button type="button" class="btn btn-dark" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Show markers</button>
+                </div>
+                </div>
+        </div>
 
+    </div>
 </div>
+    <form  method="post" action="caseEntry.php">
+    <!-- Modal -->
+    <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="staticBackdropLabel">Complete visitation entry</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <div class="mb-3">
+                        <label for="exampleFormControlInput1" class="form-label">Entry of visitation in this poi.Please insert approximate people who visited this exact location.</label>
+                        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="number of approximation">
+                    </div>
+
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <input type="submit"  name="case_entry" class="btn btn-primary" value="Submit"/>
+                </div>
+            </div>
+        </div>
+    </div>
+    </form>
 </body>
 </html>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
