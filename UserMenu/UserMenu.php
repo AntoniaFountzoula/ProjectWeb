bc9b2f2707e6f0873c0fa8e03ff65ea0b2240ad7
AntoniaFntz <95241834+AntoniaFountzoula@users.noreply.github.com>
2022-09-15 18:45:41 +0300

    Debugging the map.js

--- UserMenu/UserMenu.php
+++ UserMenu/UserMenu.php
@@ -29,7 +29,7 @@ if (!isset($_SESSION['user']))
     </script>
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script type="text/javascript" src="map.js"></script>
+
 
     <title>Covid-19FinDer</title>
 </head>
@@ -140,7 +140,7 @@ if (!isset($_SESSION['user']))
 </body>
 </html>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
+<script type="text/javascript" src="map.js"></script>
 <script>
 
 </script>
--- UserMenu/map.js
+++ UserMenu/map.js
@@ -1,106 +1,97 @@
-var map;
+// Initialize the map
+var  map = L.map('map');
+// Get the tile layer from OpenStreetMaps
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+    {
+        MaxZoom: 13,
+        // Set the attribution for OpenStreetMaps
+        attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+
+    }).addTo(map);
+
+var pos;
 navigator.geolocation.getCurrentPosition(function (location) {
-        // Initialize the map
-        map = L.map('map');
         var latlng = new L.LatLng(location.coords.latitude, location.coords.longitude);
-        var pos={'lat':location.coords.latitude, 'long':location.coords.longitude};
-
-        // Get the tile layer from OpenStreetMaps
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-            {
-                MaxZoom: 13,
-                // Set the attribution for OpenStreetMaps
-                attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-
-            }).addTo(map);
-
-        // Set the view of the map
-        // with the latitude, longitude and the zoom value
-        map.setView(latlng, 13);
-
+         pos={'lat':location.coords.latitude, 'long':location.coords.longitude};
         // Show a market at the position
         let myposition = L.marker(latlng).addTo(map);
-
+    // Set the view of the map
+    // with the latitude, longitude and the zoom value
+    map.setView(latlng, 13);
         // Bind popup to the marker with a popup
         myposition.bindPopup("Here I am");
         // L.circle(latlng, {radius: 5000}).addTo(map);
      L.circle(latlng,{radius: 20, color: 'green', fillColor: '#00ff33', fillOpacity: 0.5}).addTo(map);
 
-        let markerArray = Array();
-        let search_markArray = Array();
+    });
+let markerArray = Array();
+let search_markArray = Array();
 
-        $("#show_poi").click(function () {
-            deleteitem(search_markArray, map);
-            if (markerArray.length !== 0) {
-                deleteitem(markerArray, map);
-            } else {
-                $.ajax({
-                    type: 'post',
-                   // async: false,
-                    url: '../UserMenu/actions/caseEntry.php',
-                    dataType: 'json',
-                    data: pos,
-                    success: function (response) {
-                        if (response.length !== 0) {
-                            for (var i = 0; i < response.length; i++) {
-                                var xy = new L.LatLng(response[i].lat, response[i].lng)
-                                let marker = new L.marker(xy).addTo(map).bindPopup("<strong>Name:</strong> " + response[i].name + "</br> <strong>Address: </strong>" + response[i].address + " </br></br> <button type=\"button\" class=\"btn btn-secondary btn-sm\" data-bs-toggle=\"modal\"  onclick='submit_visit()'  data-bs-target=\"#staticBackdrop\" ><div class='v_button' value="+response[i].id+">Submit Visit</div></button> ");
-                                markerArray.push(marker);
-                            }
-                        }
-                        else{
-                            alert('There are not PIO close to you!');
-                        }
-
-                        console.log(markerArray);
-
-                    },
-                    error: function (error) {
-                        console.log(error);
-                    }
-                });
-            }
-
-
-        });
-
-    $("#search").click(function (){
-        deleteitem(search_markArray, map);
-        let category= document.getElementById('category').value;
+$("#show_poi").click(function () {
+    deleteitem(search_markArray, map);
+    if (markerArray.length !== 0) {
+        deleteitem(markerArray, map);
+    } else {
         $.ajax({
             type: 'post',
             // async: false,
-            url: '../UserMenu/actions/search.php',
+            url: '../UserMenu/actions/caseEntry.php',
             dataType: 'json',
-            data: {'category':category},
+            data: pos,
             success: function (response) {
                 if (response.length !== 0) {
-                    console.log(response);
                     for (var i = 0; i < response.length; i++) {
-                        var xy = new L.LatLng(response[i].lat, response[i].lng)
-                        let marker = new L.marker(xy).addTo(map).bindPopup("<strong>Name:</strong> " + response[i].name + "</br> <strong>Address: </strong>" + response[i].address + " </br></br> <button type=\"button\" class=\"btn btn-secondary btn-sm\" data-bs-toggle=\"modal\"  onclick='submit_visit()'  data-bs-target=\"#staticBackdrop\" ><div class='v_button' value="+response[i].id+">Submit Visit</div></button> ");
-                         search_markArray.push(marker);
+                        var xy = new L.LatLng(response[i].lat, response[i].lng);
+                        let contex="<strong>Name:</strong> " + response[i].name + "</br> <strong>Address: </strong>" + response[i].address;
+                        contex+=" "
+                        contex+=" </br></br> <button type=\"button\" class=\"btn btn-secondary btn-sm\" data-bs-toggle=\"modal\"  onclick='submit_visit()'  data-bs-target=\"#staticBackdrop\" ><div class='v_button' value="+response[i].id+">Submit Visit</div></button> ";
+                        let marker = new L.marker(xy).addTo(map).bindPopup(contex);
+                        markerArray.push(marker);
                     }
                 }
                 else{
-                    alert('There are not PIO for the category '+category+' !');
+                    alert('There are not PIO close to you!');
                 }
 
+                console.log(markerArray);
 
             },
             error: function (error) {
                 console.log(error);
             }
         });
+    }
+});
+
+$("#search").click(function (){
+    deleteitem(search_markArray, map);
+    let category= document.getElementById('category').value;
+    $.ajax({
+        type: 'post',
+        // async: false,
+        url: '../UserMenu/actions/search.php',
+        dataType: 'json',
+        data: {'category':category},
+        success: function (response) {
+            if (response.length !== 0) {
+                console.log(response);
+                for (var i = 0; i < response.length; i++) {
+                    var xy = new L.LatLng(response[i].lat, response[i].lng)
+                    let marker = new L.marker(xy).addTo(map).bindPopup("<strong>Name:</strong> " + response[i].name + "</br> <strong>Address: </strong>" + response[i].address + " </br></br> <button type=\"button\" class=\"btn btn-secondary btn-sm\" data-bs-toggle=\"modal\"  onclick='submit_visit()'  data-bs-target=\"#staticBackdrop\" ><div class='v_button' value="+response[i].id+">Submit Visit</div></button> ");
+                    search_markArray.push(marker);
+                }
+            }
+            else{
+                alert('There are not PIO for the category '+category+' !');
+            }
+
+
+        },
+        error: function (error) {
+            console.log(error);
+        }
     });
-
-
-
-
-    });
-
-
-
+});
 
 
 function closure(marker,map){
