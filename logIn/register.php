140ed41b4e90f40492e7f9ea3ba45397f2068fd8
AntoniaFntz <95241834+AntoniaFountzoula@users.noreply.github.com>
2022-09-18 18:07:16 +0300

    Impement register form with ajax

--- logIn/register.php
+++ logIn/register.php
@@ -1,19 +1,14 @@
 <?php
-$servername ="localhost";
-$username = "root";
-$password ="";
-$dbname ="project_web";
-
 // Create connection
-$conn = new mysqli($servername, $username, $password,$dbname);
+$conn = new mysqli("localhost", "root", "","project_web");
 
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-$email_input= $_REQUEST['email'];
-$Username= $_REQUEST['username'];
-$Password=$_REQUEST['password'];
+$email_input= $_POST['email'];
+$Username= $_POST['username'];
+$Password=$_POST['password'];
 //Check if the user already exist
 $query1="SELECT email FROM user where email= '$email_input'";
 $results = mysqli_query($conn, $query1);
@@ -22,20 +17,17 @@ if(mysqli_num_rows($results) == 0)
 {
     $sql = " INSERT INTO user (user_type,username,email,password) VALUES ('user','$Username','$email_input','$Password') ";
     if (mysqli_query($conn, $sql)) {
-        echo "<script>alert('Welcome to Covid-19 Finder! You have been register successful!');
-            window.location.href='logIn.html';
-            </script>";
+        echo json_encode(array("Status"=>"success"));
 
-
-    } else {
-        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+    else
+    {
+        echo json_encode(array("Status"=>mysqli_error($conn)));
     }
     die();
 }
 else{
-    echo "<script>alert('You already have an account with this email!');
-            window.location.href='register.html';
-            </script>";
+    echo json_encode(array("Status"=>"You already have an account with this email!"));
     die();
 }
 
